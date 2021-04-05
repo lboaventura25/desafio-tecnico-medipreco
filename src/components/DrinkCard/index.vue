8260dc169d3806861db86f25dd9fba99884ef65e
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-04-05 19:18:07 -0300

    Adiciona grid com imagens vindas da api e conteudo da pagina

---
+++ src/components/DrinkCard/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <router-link to="/drink" class="drink" v-bind:class="{'inline': !grid}">
+    <img :src="drink.strDrinkThumb" :alt="drink.strDrink" />
+    <figcaption>
+      <p>{{ drink.strDrink }}</p>
+      <span>{{ drink.idDrink }}</span>
+    </figcaption>
+  </router-link>
+</template>
+
+<script>
+
+export default {
+  name: 'DrinkCard',
+  props: {
+    drink: {
+      strDrink: String,
+      strDrinkThumb: String,
+      idDrink: String
+    },
+    grid: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+.drink {
+  min-width: 310px;
+  border: 1px solid #E5E9EB;
+}
+
+.inline {
+  width: 100%;
+  min-width: 500px;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid #E5E9EB;
+}
+
+.drink img {
+  width: 100%;
+}
+
+.inline img {
+  max-width: 70%;
+}
+
+figcaption {
+  margin: 20px 0 44px 24px;
+}
+
+figcaption p {
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 24px;
+  color: #252C32;
+}
+
+figcaption span {
+  font-weight: 600;
+  font-size: 1.8rem;
+  line-height: 24px;
+  color: #252C32;
+}
+
+</style>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -4,20 +4,61 @@
     <main>
       <article>
         <head>
-          <h1>Os melhores drinks não-alcoólicos do <span class="blue-text">Brasil</span></h1>
+          <h1>Os melhores drinks não-alcoólicos do 
+            <span class="blue-text">Brasil</span>
+          </h1>
+          <section class="buttons">
+            <a class="button" v-bind:class="{'active-left': grid}" v-on:click="switchGrid">
+              <img src="../../assets/Grid.png" alt="Grid" />
+            </a>
+            <a class="button" v-bind:class="{'active-right': !grid}" v-on:click="switchGrid" >
+              <img src="../../assets/Column.png" alt="Colunas" />
+            </a>
+          </section>
         </head>
+        <small>{{ drinksLenght }} resultados encontrados</small>
       </article>
+      <section class="drinks" v-bind:class="{'no-grid': !grid}">
+        <DrinkCard 
+          v-for="(drink, index) in drinks" 
+          :key="index"
+          :drink="drink"
+          :grid="grid"
+        />
+      </section>
     </main>
   </div>
 </template>
 
 <script>
 import HomeHeader from '@/components/HomeHeader/index';
+import DrinkCard from '@/components/DrinkCard/index';
+
+import api from '@/services/api';
 
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    DrinkCard
+  },
+  data() {
+    return {
+      drinksLenght: 0,
+      drinks: [],
+      grid: true
+    };
+  },
+  mounted() {
+    api.get('/filter.php?a=Non_Alcoholic').then(response => {
+      this.drinks = response.data.drinks;
+      this.drinksLenght = this.drinks.length;
+    })
+  },
+  methods: {
+    switchGrid: function() {
+      this.grid = !this.grid;
+    },
   }
 }
 
@@ -26,20 +67,21 @@ export default {
 <style scoped>
 
 main {
-  width: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
 
 article {
   width: 80%;
-  min-height: calc(100vh - 78px);
   display: flex;
+  flex-direction: column;
   padding-top: 17px;
 }
 
 head {
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -48,11 +90,77 @@ head {
 h1 {
   font-weight: 600;
   font-size: 2.4rem;
+  line-height: 32px;
   color: #252C32;
 }
 
 h1 .blue-text {
   color: #4094F7;
+}
+
+.buttons {
+  height: 32px;
+  background-color: #F6F8F9;
+  display: flex;
+  flex-direction: row;
+  border-radius: 6px;
+}
+
+.button {
+  padding: 9px;
+}
+
+.active-left {
+  background-color: #E5E9EB;
+  border-radius: 6px 0 0 6px;
+}
+
+.active-right {
+  background-color: #E5E9EB;
+  border-radius: 0 6px 6px 0;
+}
+
+small {
+  font-weight: 500;
+  font-size: 1.8rem;
+  line-height: 32px;
+  color: #9AA6AC;
+}
+
+.drinks {
+  width: 60%;
+  margin-top: 29px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  justify-content: center;
+}
+
+.no-grid {
+  display: flex;
+  flex-direction: column;
+}
+
+@media(max-width: 800px) {
+  h1 {
+    max-width: 70%;
+  }
+}
+
+@media (max-width: 1000px) {
+  .drinks {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
+}
+
+@media (max-width: 670px) {
+  .drinks {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 }
 
 </style>
